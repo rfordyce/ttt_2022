c984a01cc128d0955e234d2d3935e9a866c3c595
rfordyce <rfordyce@users.noreply.github.com>
2022-04-22 22:33:56 -0500

    backend: wrap cv2.VideoCapture() in context manager (stops nasty clicking) and some housekeeping

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+from contextlib import contextmanager
 
 import cv2  # for board reading - should this be trained or ugly dynamic?
 # import numpy as np
@@ -12,21 +13,20 @@ from redis import Redis  # for distributed queue
 #   otherwise feed into redis queue --> display on API/UI via socket?..
 
 
-def read_board(webcam_device):
-    # https://stackoverflow.com/a/56354464/
-    # TODO rewrite as a context manager if using open:read:close strategy
-    cap = cv2.VideoCapture(webcam_device)  # pass device?
-    ret, frame = cap.read()  # read one frame
-    cap.release()  # release the VideoCapture object
-    # some detection if the frame is trash (can this happen?)
-    # what if the webcam is slow?
-    # what if the webcam is out of focus (mine probably has autofocus)
-    #   honestly, less focus may be better for this ugly detection anyways
+@contextmanager
+def capture_device(webcam_device="/dev/video0"):
+    cap = cv2.VideoCapture(webcam_device)
+    try:
+        yield cap
+    finally:
+        cap.release()
 
-    print(frame.shape)  # how to pretty print
-    print(frame)  # how to pretty print
-    # print(np.matrix(frame))
-    return frame
+
+def read_board(webcam):
+    ret, frame = webcam.read()  # read one frame
+    # convert to board
+    board = []
+    return frame, board
 
 
 def surrender_loop():
@@ -46,25 +46,23 @@ def img_raw_b64(img):
     return base64.b64encode(buffer)
 
 
-def main(webcam_device):
+def main():
     # offer to discover webcam device?
     redis_handle = Redis(os.environ["ADDRESS_CACHE"])
 
-    while True:
-        # read Q (redis)? (may have explicit moves?)
-        state_board = read_board(webcam_device)  # FIXME do we need to pass device?
-        # redis_handle(state_board)  # XXX just for display on UI
-        redis_handle.set("webcam_view", img_raw_b64(state_board))
-        # redis_handle.set("webcam_view", "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==")  # XXX just for display on UI
-        if not "check if it's my turn":
-            time.sleep(1)  # TODO async?
-            continue  # next iteration
-        time.sleep(600)  # XXX make a long loop - camera clicks uncomfortably
-        "check if game is lost"  # --> surrender action loop
-        "calculate next move"
-        "make next move"
-        "verify move was made correctly"  # stretch goal? --> may help debug high-level
-        # feed back to Q at end of loop?
+    with capture_device() as webcam:
+        while True:
+            # read Q (redis)? (may have explicit moves?)
+            frame, board = read_board(webcam)
+            redis_handle.set("webcam_view", img_raw_b64(frame))
+            if not "check if it's my turn":
+                time.sleep(1)  # TODO async?
+                continue  # next iteration
+            "check if game is lost"  # --> surrender action loop
+            "calculate next move"
+            "make next move"
+            "verify move was made correctly"  # stretch goal? --> may help debug high-level
+            # feed back to Q at end of loop?
 
 
 if __name__ == "__main__":
@@ -78,4 +76,4 @@ if __name__ == "__main__":
     #   still, it's probably better to continuously capture frames and detect in them
     #   perhaps snagging motion and combining them for something
     #   suspect this will lead to much better detection
-    main("/dev/video0")
+    main()
